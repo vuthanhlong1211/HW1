0ce2c84344b6a878744d41cafa6a386029ba1d58
vuthanhlong1211 <21020647@vnu.edu.vn>
2024-04-20 13:44:21 +0700

    add hw2

---
+++ hw2/ex212.py
@@ -0,0 +1,93 @@
+import numpy as np
+from skimage import io as io_url
+import matplotlib.pyplot as plt
+import cv2
+
+
+
+def DFT_slow(data):
+  """
+  Implement the discrete Fourier Transform for a 1D signal
+  params:
+    data: Nx1: (N, ): 1D numpy array
+  returns:
+    DFT: Nx1: 1D numpy array 
+  """
+  N = data.shape[0]
+  DFT = np.zeros(data.shape, dtype=complex)
+  
+  for n in range(N):
+    for m in range(N):
+      DFT[n] += data[m]/(np.exp(2j * np.pi * n * m / N))
+    DFT[n] /= N
+  return DFT
+    
+
+
+def show_img(origin, row_fft, row_col_fft):
+    """
+    Show the original image, row-wise FFT and column-wise FFT
+
+    params:
+        origin: (H, W): 2D numpy array
+        row_fft: (H, W): 2D numpy array
+        row_col_fft: (H, W): 2D numpy array    
+    """
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(12, 8))
+    axs[0].imshow(origin, cmap='gray')
+    axs[0].set_title('Original Image')
+    axs[0].axis('off')
+    axs[1].imshow(np.log(np.abs(np.fft.fftshift(row_fft))), cmap='gray')
+    axs[1].set_title('Row-wise FFT')
+    axs[1].axis('off')
+    axs[2].imshow((np.log(np.abs(np.fft.fftshift(row_col_fft)))), cmap='gray')
+    axs[2].set_title('Column-wise FFT')
+    axs[2].axis('off')
+    plt.show()
+
+
+def DFT_2D(gray_img):
+    """
+    Implement the 2D Discrete Fourier Transform
+    Note that: dtype of the output should be complex_
+    params:
+        gray_img: (H, W): 2D numpy array
+        
+    returns:
+        row_fft: (H, W): 2D numpy array that contains the row-wise FFT of the input image
+        row_col_fft: (H, W): 2D numpy array that contains the column-wise FFT of the input image
+    """
+    src = np.array(gray_img)
+    row_fft = []
+
+    h, w = gray_img.shape
+
+    for i in range(h):
+      temp = np.fft.fft(src[i, 0 : w])
+        # temp = DFT_slow(src[i, 0 : W])
+      row_fft.append(temp)  
+
+    row_fft = np.array(row_fft)
+    src2 = np.zeros(gray_img.shape, dtype=complex)
+    np.copyto(src2, row_fft.transpose())
+
+    for i in range (w):
+      src2[i] = np.fft.fft(src2[i])
+    row_col_fft = np.transpose(src2)
+    return row_fft, row_col_fft 
+         
+if __name__ == '__main__':
+  
+    # ex1
+    # x = np.random.random(1024)
+    # print(np.allclose(DFT_slow(x), np.fft.fft(x)))
+  # ex2
+    img = io_url.imread('https://img2.zergnet.com/2309662_300.jpg')
+    gray_img = np.mean(img, -1)
+    row_fft, row_col_fft = DFT_2D(gray_img)
+    show_img(gray_img, row_fft, row_col_fft)
+
+ 
+
+
+
